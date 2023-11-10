dc2e81d7a378d4e014fb9fb740fcdfcabaf73676
GerenciaS <gerencia.sistemas@mercadodeimportaciones.com>
2023-11-10 11:51:59 -0700

    Se sube Repositorio de ordenes de servicio correspondiente al codigo encontrado en los respaldo entregados a Gerencia Sistemas

---
+++ Actualizador/Actualizador.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Actualizador
+{
+    partial class Actualizador
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Actualizador));
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscarRuta = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(318, 18);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(94, 26);
+            this.btnActualizar.TabIndex = 0;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Enabled = false;
+            this.txtRuta.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRuta.Location = new System.Drawing.Point(38, 94);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(538, 22);
+            this.txtRuta.TabIndex = 1;
+            this.txtRuta.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ruta:";
+            this.label1.Visible = false;
+            // 
+            // btnBuscarRuta
+            // 
+            this.btnBuscarRuta.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarRuta.Location = new System.Drawing.Point(578, 95);
+            this.btnBuscarRuta.Name = "btnBuscarRuta";
+            this.btnBuscarRuta.Size = new System.Drawing.Size(24, 20);
+            this.btnBuscarRuta.TabIndex = 3;
+            this.btnBuscarRuta.Text = "...";
+            this.btnBuscarRuta.UseVisualStyleBackColor = true;
+            this.btnBuscarRuta.Visible = false;
+            this.btnBuscarRuta.Click += new System.EventHandler(this.btnBuscarRuta_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 19);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(303, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // Actualizador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 62);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtRuta);
+            this.Controls.Add(this.btnBuscarRuta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnActualizar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Actualizador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Actualizador";
+            this.Load += new System.EventHandler(this.Actualizador_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscarRuta;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Actualizador/Actualizador.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Actualizador
+{
+    public partial class Actualizador : Form
+    {
+
+        public Actualizador()
+        {
+            InitializeComponent();
+        }
+        private void PlActualizar()
+        {
+            foreach (Process proceso in Process.GetProcesses())
+            {
+                if (proceso.ProcessName == "ModuloConexionesRemotas")
+                {
+                    proceso.Kill();
+                }
+            }
+        }
+        private void PlIniciarAplicacion()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = txtRuta.Text+ "\\ModuloConexionesRemotas.exe";
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            Process processTemp = new Process();
+            processTemp.StartInfo = startInfo;
+            processTemp.EnableRaisingEvents = true;
+            try
+            {
+                processTemp.Start();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            this.btnActualizar.Enabled = false;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 3;
+            progressBar1.Value = 1;
+            PlActualizar();
+            Thread.Sleep(6000);
+            WebClient mywebClient = new WebClient();
+            mywebClient.DownloadFile("http://repositorio.appsmercadodeimportaciones.com/SistemaConexionesRemotas/ModuloConexionesRemotas.exe",""+txtRuta.Text+"\\ModuloConexionesRemotas.exe");
+            mywebClient.DownloadFile("http://repositorio.appsmercadodeimportaciones.com/SistemaConexionesRemotas/ModuloConexionesRemotas.exe.config","" + txtRuta.Text + "\\ModuloConexionesRemotas.exe.config");
+            progressBar1.Value = 2;
+            mywebClient.DownloadFile("http://repositorio.appsmercadodeimportaciones.com/SistemaConexionesRemotas/MySql.Data.dll", "" + txtRuta.Text + "\\MySql.Data.dll");
+            progressBar1.Value = 3;
+            this.btnActualizar.Enabled = true;
+            MessageBox.Show("Modulo actualizado correctamente.", "Remoto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            PlIniciarAplicacion();
+            progressBar1.Value = 0;
+            this.Dispose();
+        }
+
+        private void btnBuscarRuta_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txtRuta.Text = fbd.SelectedPath;
+        }
+
+        private void Actualizador_Load(object sender, EventArgs e)
+        {
+            txtRuta.Text = Directory.GetCurrentDirectory();
+        }
+    }
+}
